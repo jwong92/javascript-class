c1e68511a92c705c584c5c6ebb98ebdb99dce865
Jessica Wong <jessica.pearl.wong@gmail.com>
2018-01-01 14:34:56 -0500

    Added form validation

--- careers/careers.js
+++ careers/careers.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 // Clear the form
-$(".input-row input, textarea").val("");
 
 //POP UP BOX
 // number of jobs
@@ -9,7 +8,6 @@ var jobClass = ".";
 
 //Determine the class that was clicked
 $("li a").on("click", function(){
-  $("#sbt-mssg").addClass("hidden");
   jobClass += $(this).attr("class");
   $(".pop-up").removeClass("hidden");
   fillJobDescr(jobClass);
@@ -38,13 +36,66 @@ $("#close").on("click", function(){
 });
 
 // SUBMIT BUTTON
-$("#sbt-btn").on("click", function(){
-  $("#sbt-mssg").removeClass("hidden");
-  $(".input-row input, textarea").val("");
-})
+// $("#sbt-btn").on("click", function(){
+//   $("#sbt-mssg").html("Thank you for applying");
+// })
 
 $("#sbt-btn").on("mouseover",function(){
   $(this).css("cursor", "pointer");
 })
+
+//FORM VALIDATION
+
+var myForm = document.forms["application-form"];
+myForm.onsubmit = processForm;
+
+function processForm(e){
+  $("sbt-mssg"). html("");
+
+  //Capture form values
+  var formControls = myForm.elements;
+  var inName = formControls[1].value;
+  var inPhone = formControls[2].value;
+  var inEmail = formControls[3].value;
+
+  // Validate form values
+  // NAME
+  if (inName === "" || inName === null) {
+    $("#sbt-mssg").html("* Please fill all required fields.");
+    return false;
+  }
+
+  //PHONE
+  var phoneNum = /^\d{10}$/;
+  if (inPhone === "" || inPhone === null) {
+    $("#sbt-mssg").html("* Please fill all required fields.");
+    return false;
+  } else if (!phoneNum.test(inPhone)) {
+    $("#sbt-mssg").html("Please enter a valid phone number.");
+    return false;
+  }
+
+  // EMAIL
+  var emailFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+
+  if (inEmail === "" || inEmail === null) {
+    $("#sbt-mssg").html("* Please fill all required fields.");
+    return false;
+  } else if (!emailFilter.test(inEmail)) {
+    $("#sbt-mssg").html("Please enter a valid email address.");
+    return false;
+  }
+
+  $("#sbt-mssg"). html("Thank you for applying!");
+  $(".input-row input, textarea").val("");
+
+
+  e.preventDefault();
+
+
+
+
+}
 
 }); //END PAGE LOAD
